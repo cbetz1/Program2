17b85a38516a56d5c410e3f43360c11c112ff149
cbetz1 <cbetz@svsu.edu>
2017-10-17 20:23:11 -0400

    ;)

--- app/src/main/java/com/example/chad/program2/TopLevelActivity.java
+++ app/src/main/java/com/example/chad/program2/TopLevelActivity.java
@@ -3,16 +3,28 @@ package com.example.chad.program2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class TopLevelActivity extends Activity {
 
+    private ShareActionProvider shareActionProvider;
+
+    public TopLevelActivity(ShareActionProvider shareActionProvider) {
+        this.shareActionProvider = shareActionProvider;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
         //Create an OnItemClickListener
         AdapterView.OnItemClickListener itemClickListener =
                 new AdapterView.OnItemClickListener(){
@@ -41,4 +53,24 @@ public class TopLevelActivity extends Activity {
         ListView listView = (ListView) findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
     }
+
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
+    }
+
+   /* @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+*/
 }
